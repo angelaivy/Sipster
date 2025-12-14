48058d750d4906863e9ea2a6a7bdc71d4c59a636
Angela Ivy <angelaivy1111@gmail.com>
2025-12-13 17:27:41 -0800

    generate recipe with full drink details

--- Drink.js
+++ Drink.js
@@ -1,19 +1,64 @@
 export class Drink {
-  constructor(name, thumb, container) {
-    this.name = name;
-    this.thumb = thumb;
+  constructor(details, container) {
+    Object.assign(this, details);
+    this.ingredients = [];
     this.container = container;
   }
 
+  // Basic details for list view.
   generateDrink() {
-    const detailShort = `
-      <li class="detailShort">
+    const detailList = `
+      <li class="detailList">
         <a href="details.html">
           <h3>${this.name}</h3>
           <img src="${this.thumb}/small" alt=""/>
         </a>
       </li>
     `
-    this.container.insertAdjacentHTML('afterbegin', detailShort);
+    
+    this.container.insertAdjacentHTML('afterbegin', detailList);
+  }
+
+  // Full recipe.
+  generateRecipe() {
+    const detailRecipe = `
+      <div class="detailRecipe">
+        <h2>${this.name}</h2>
+        <img src="${this.thumb}/small" alt=""/>
+        
+        <h3>What you'll need</h3>
+        <ul class="ingredients">
+          <li>${this.glass}</li>
+        </ul>
+
+        <h3>Instructions</h3>
+        <p>${this.instructions}</p>
+      </div>
+    `
+    // Generate the ingredients and measurements as list items.
+    this.container.insertAdjacentHTML('beforeend', detailRecipe);
+    const detailRecipeContainer = document.querySelector('.detailRecipe');
+    const glass = detailRecipeContainer.querySelector('.ingredients > li:first-child');
+    this.ingredients.forEach((ingredient) => {
+      const newIngredient = `<li>${ingredient}</li>`;
+      glass.insertAdjacentHTML('beforeend', newIngredient);
+    });
+  }
+
+  // Add ingredients + measurements to the array.
+  addIngredient(ingredient) {
+    this.ingredients.push(ingredient);
+  }
+
+  // Get drink name and save in localstorage for later use.
+  getDrinkName() {
+    const drinkLink = document.querySelector('.detailList a');
+    drinkLink.addEventListener('click', async (e) => {
+      const anchor = e.target.closest('a');
+      if (anchor) {
+        const name = anchor.querySelector('h3').innerText;
+        localStorage.setItem('selectedDrink', name);
+      }
+    })
   }
 }
--- sipster.js
+++ sipster.js
@@ -4,9 +4,11 @@ import { searchDrinks  } from './search.js';
 import { isValidInput  } from './validation.js';
 import { Drink } from './Drink.js';
 
-const inputSelect = document.getElementById('searchInput');
-const filterSelect = document.getElementById('selectFilter');
-const detailsContainer = document.getElementById('results');
+const inputSelect = document.getElementById('searchInput'),
+      filterSelect = document.getElementById('selectFilter'),
+      resultsContainer = document.getElementById('results'),
+      searchButton = document.querySelector('[type="submit"]'),
+      detailsContainer = document.querySelector('main#drinkDetails');
 let query,
     path;
 
@@ -44,12 +46,10 @@ filterSelect?.addEventListener('change', () => {
   }
 });
 
-// Search and get results.
-const searchButton = document.querySelector('[type="submit"]');
 searchButton?.addEventListener('click', async (e) =>{
   e.preventDefault();
   // Clear out the container before the next query.
-  detailsContainer.innerText = '';
+  resultsContainer.innerText = '';
   const results = await searchDrinks(path, query);
   const drinks = results['drinks'];
   console.log('insearchbtn', results);
@@ -57,19 +57,69 @@ searchButton?.addEventListener('click', async (e) =>{
   if (Array.isArray(drinks)) {
     // Generate the unordered list element.
     const drinkList = document.createElement('ul');
-    detailsContainer.prepend(drinkList)
+    resultsContainer.prepend(drinkList)
     // Get drinks and populate with results.
     drinks.forEach((drink) => {
-      const getDrink = new Drink(
-        drink.strDrink,
-        drink.strDrinkThumb,
+      const getDrink = new Drink({
+          name: drink.strDrink,
+          thumb: drink.strDrinkThumb,
+        },
         drinkList
       );
       getDrink.generateDrink();
+      getDrink.getDrinkName();
     });
   } else {
-    detailsContainer.innerText = 'No data found, try another search.';
+    resultsContainer.innerText = 'No data found, try another search.';
   }
- 
 })
 
+// Populate details container on details page.
+if (detailsContainer) {
+  const clickedDrinkName = localStorage.getItem('selectedDrink');
+  let selectedDrinks = [];
+  try {
+    // There could be multiple drinks that use name.
+    const results = await searchDrinks('search.php?s=', `${clickedDrinkName}`);
+    selectedDrinks = results['drinks'];
+    console.log(selectedDrinks);
+  } catch(e) {
+    console.log('Drink query invalid.');
+  }
+
+  // If there are valid results display the list - should be an array.
+  if (Array.isArray(selectedDrinks)) {
+    selectedDrinks.forEach((selectedDrink) => {
+      console.log('selectedDrink', selectedDrink)
+      let ingredients = [];
+      let measurements = [];
+
+      // Get the ingredients/measurements if they have a value.
+      for (const key in selectedDrink) {
+        if (key.startsWith('strIngredient') && selectedDrink[key]) {
+          ingredients.push(selectedDrink[key]);
+        }
+        if (key.startsWith('strMeasure') && selectedDrink[key]) {
+          measurements.push(selectedDrink[key])
+        }
+      }
+
+      const ingredientsWithMeasurements = ingredients.map((item, index) => {
+        return `${measurements[index]}${item}`
+      })
+
+      const getRecipe = new Drink({
+          name: selectedDrink.strDrink,
+          thumb: selectedDrink.strDrinkThumb,
+          glass: selectedDrink.strGlass,
+          instructions: selectedDrink.strInstructions,
+        },
+        document.querySelector('#drinkDetails')
+      )
+      for (const key in ingredientsWithMeasurements) {
+        getRecipe.addIngredient(ingredientsWithMeasurements[key]);
+      }
+      getRecipe.generateRecipe();
+    });
+  }
+}
