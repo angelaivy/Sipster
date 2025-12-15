08f2a086b68b90bb31d8b6fb354fbf84dcd7be96
Angela Ivy <angelaivy1111@gmail.com>
2025-12-14 18:03:55 -0800

    i am once again re-doing the test suite. Less mocking, test functions

--- sipster.spec.js
+++ sipster.spec.js
@@ -1,85 +1,24 @@
 import { isValidInput } from './validation.js';
-import { searchDrinks } from './search.js';
- // Select filter, save value on change
-// Type user input, save value on change
-// Click search button
-// - validate that filter is selected, 
-// - validate that input has value
-// - take values and build the endpoint path and query
-// - searchDrinks(query)
-//  - make the API call with the endpoint and return response.
-//  - populate results container with drinnks previews
-//  - if no results, show error to user
-// Drink preview is Image and Name, linked 
+import { doApiRequest } from './api.js';
+import { Drink } from './Drink.js';
+import { FavoritesManager } from './FavoritesManager.js';
+
 describe("Filter and Search", function() {
-    let mockInputElement,
-        mockSelectElement,
-        mockErrorElement,
-        parentDiv;
-
-    beforeAll(function() {
-        parentDiv = document.createElement('div');
-
-        mockErrorElement = document.createElement('div');
-        mockErrorElement.className = 'errorMessage';
-
-        mockInputElement = document.createElement('input');
-        mockInputElement.id = 'searchInput';
-
-        mockSelectElement = document.createElement('select');
-        mockSelectElement.id = 'selectFilter';
-
-        parentDiv.appendChild(mockSelectElement);
-        parentDiv.appendChild(mockInputElement);
-        parentDiv.appendChild(mockErrorElement);
-        document.body.appendChild(parentDiv);
+    it('should return false for invalid input', function() {
+        expect(isValidInput('text', null, '')).toBe(false);
+        expect(isValidInput('text', null, '!')).toBe(false);
+        expect(isValidInput('select-one', null, '')).toBe(false);
     });
 
-    afterAll(function() {
-        parentDiv.remove();
-        mockInputElement.remove();
-        mockSelectElement.remove();
-        mockErrorElement.remove();
+    it('should return true for valid input', function() {
+        expect(isValidInput('text', null, 'margarita')).toBe(true);
+        expect(isValidInput('select-one', null, 'name')).toBe(true);
     });
 
-    it("should return false and show error when input is invalid", function() {
-        const invalidInputs = ['', '!', '@#$%'];
-        invalidInputs.forEach(input => {
-            const result = isValidInput(mockInputElement.type, mockInputElement, input);
-            expect(result).toBe(false);
-            expect(mockErrorElement.classList.contains('show')).toBe(true);
-            expect(mockErrorElement.classList.contains('hide')).toBe(false);
-        });
-    });
-
-    it("should return true and hide error when input is valid", function() {
-        const validInputs = ['margarita', 'a', 'gin'];
-        validInputs.forEach(input => {
-            const result = isValidInput(mockInputElement.type, mockInputElement, input);
-            expect(result).toBe(true);
-            expect(mockErrorElement.classList.contains('show')).toBe(false);
-            expect(mockErrorElement.classList.contains('hide')).toBe(true);
-        });
-    });
-
-    it("should return false and show error when select is invalid", function() {
-        const result = isValidInput(mockSelectElement.type, mockSelectElement, '');
-        expect(result).toBe(false);
-        expect(mockErrorElement.classList.contains('show')).toBe(true);
-        expect(mockErrorElement.classList.contains('hide')).toBe(false);
-    });
-
-    it("should return true and show error when select is invalid", function() {
-        const result = isValidInput(mockSelectElement.type, mockSelectElement, 'name');
-        expect(result).toBe(true);
-        expect(mockErrorElement.classList.contains('show')).toBe(false);
-        expect(mockErrorElement.classList.contains('hide')).toBe(true);
-    });
-
-    it("should return results containing 'margarita' when name filter is selected", async function() {
+    it('should return results when name filter is selected', async function() {
         const searchPath = 'search.php?s=';
         const query = 'margarita';
-        const results = await searchDrinks(searchPath, query);
+        const results = await doApiRequest(searchPath, query);
         const drinks = results['drinks'];
         expect(results).toBeDefined();
         expect(drinks.length).toBeGreaterThan(0);
@@ -88,83 +27,123 @@ describe("Filter and Search", function() {
         });
     });
 
-    it("should return results when filtering by ingredients", async function() {
-        // This endpoint only provides id, name, and image so we should just check there are results.
-        const searchPath = 'filter.php?i=';
-        const query = 'vodka';
-        const results = await searchDrinks(searchPath, query);
+    it('should return null when drink doesn\'t exist.', async function() {
+        const searchPath = 'search.php?s=';
+        const query = 'nonexistentdrinkname';
+        const results = await doApiRequest(searchPath, query);
         const drinks = results['drinks'];
-        expect(results).toBeDefined();
-        expect(drinks.length).toBeGreaterThan(0);
+        expect(drinks).toBeNull();
     });
 });
 
-//     it("should return a list of results with name and image", async function() {
-//         const results = await searchDrinks('margarita');
-//         expect(results).toBeDefined();
-//         expect(results.length).toBeGreaterThan(0);
-//         results.forEach(drink => {
-//             expect(drink.strDrink).toBeDefined();
-//             expect(drink.strDrinkThumb).toBeDefined();
-//         });
-//     });
+describe('Drink Details', function() {
+    let detailList;
+    beforeEach(() => {
+        const container = document.createElement('div');
+        container.id = 'container';
+        document.body.append(container);
+        detailList = document.getElementById('container');
+    });
+    afterEach(() => {
+        detailList.remove();
+    });
 
-//     it("should display an error message when no results are found", async function() {
-//         const results = await searchDrinks('nonexistentdrinkname');
-//         expect(results).toBeNull();
-//         expect(getResultsContainer().innerHTML).toContain('No results found');
-//     });
-// });
+    it('should have expected drink list item html', function() {
+        const drink = new Drink({
+                id: 1234,
+                name: 'margarita',
+                thumb: 'https://www.thecocktaildb.com/images/media/drink/dztcv51598717861.jpg',
+            },
+            detailList,
+        )
+        drink.generateDrink();
+        expect(detailList.innerHTML).toContain('<li id="detailItem-1234" class="detailItem">');
+        expect(detailList.innerHTML).toContain('<h3>margarita</h3>');
+        expect(detailList.innerHTML).toContain('<img src="https://www.thecocktaildb.com/images/media/drink/dztcv51598717861.jpg/small" alt="">');
+    });
 
+    it('should have expected drink recipe html', function() {
+        const drink = new Drink({
+                id: 1234,
+                name: 'mimosa',
+                thumb: 'https://www.thecocktaildb.com/images/media/drink/juhcuu1504370685.jpg',
+                glass: 'champagne flute',
+                instructions: 'pour champagne until the glass is mostly full, splash orange juice'
+            },
+            detailList,
+           
+        )
+        drink.addIngredient('champange');
+        drink.addIngredient('orange juice');
+        drink.generateRecipe();
+               
+        expect(detailList.innerHTML).toContain('<div id="drink-1234" class="detailRecipe">');
+        expect(detailList.innerHTML).toContain('<h2>mimosa</h2>');
+        expect(detailList.innerHTML).toContain('<img src="https://www.thecocktaildb.com/images/media/drink/juhcuu1504370685.jpg/small" alt="">');
+         expect(detailList.innerHTML).toContain('<p>pour champagne until the glass is mostly full, splash orange juice</p>');
+        expect(detailList.innerHTML).toContain('<li>champagne flute</li>');
+        expect(detailList.innerHTML).toContain('<li>champange</li>');
+        expect(detailList.innerHTML).toContain('<li>orange juice</li>');
+    });
 
-// // describe("Drink Details Suite", function() {
-// //     it("should display drink details when a drink is clicked from results", async function() {
-// //       const results = await searchDrinks('margarita');
-// //       const firstDrink = results[0];
-// //       displayDrinkDetails(firstDrink.idDrink);
-// //       const detailsContainer = getDrinkDetailsContainer();
-// //       expect(detailsContainer).toBeDefined();
-// //       expect(detailsContainer.innerHTML).toContain(firstDrink.strDrink);
-// //       expect(detailsContainer.innerHTML).toContain(firstDrink.strInstructions);
-// //       expect(detailsContainer.innerHTML).toContain(firstDrink.strDrinkThumb);
-// //       expect(detailsContainer.innerHTML).toContain(firstDrink.strIngredient1);
-// //       expect(detailsContainer.innerHTML).toContain(firstDrink.strGlass);
-// //       expect(detailsContainer.innerHTML).toContain(firstDrink.Measure1);
-// //     });
-// // });
+    it('should add drink name to localStorage', function() {
+         const drink = new Drink({
+                id: 1234,
+                name: 'Mimosa',
+                thumb: 'https://www.thecocktaildb.com/images/media/drink/juhcuu1504370685.jpg',
+            },
+            detailList,
+        )
+        drink.generateDrink();
+        drink.getDrinkName();
+        expect(localStorage.getItem('selectedDrink')).toBe('Mimosa');
+    })
+});
 
-// // describe("Random Drink Suite", async function() {
-// //     it("should display a random drink when the random drink button is clicked", function() {
-// //         clickRandomDrinkButton();
-// //         const detailsContainer = getDrinkDetailsContainer();
-// //         expect(detailsContainer).toBeDefined();
-// //         expect(detailsContainer.innerHTML).toContain('strDrink');
-// //         expect(detailsContainer.innerHTML).toContain('strInstructions');
-// //         expect(detailsContainer.innerHTML).toContain('strDrinkThumb');
-// //         expect(detailsContainer.innerHTML).toContain('strIngredient1');
-// //         expect(detailsContainer.innerHTML).toContain('strGlass');
-// //         expect(detailsContainer.innerHTML).toContain('strMeasure1');
-// //     });
-// // }); 
+describe('Favorites Manager', function() {
+    let detailList;
+    beforeEach(() => {
+        const container = document.createElement('div');
+        container.id = 'container';
+        document.body.append(container);
+        detailList = document.getElementById('container');
+    });
+    afterEach(() => {
+        detailList.remove();
+    });
 
-// // describe("Favorites Suite", function() {
-// //     it("should add a drink to favorites list", async function() {
-// //         const results = await searchDrinks('margarita');
-// //         const firstDrink = results[0];
-// //         addToFavorites(firstDrink.idDrink);
-// //         const favoritesList = getFavoritesList();
-// //         expect(favoritesList).toBeDefined();
-// //         expect(favoritesList.length).toBeGreaterThan(0);
-// //         const isInFavorites = favoritesList.some(drink => drink.idDrink === firstDrink.idDrink);
-// //         expect(isInFavorites).toBeTrue();
-// //     });
+    it('adds drink to favorites', function() {
+        const drink = new Drink({
+                id: 1234,
+                name: 'Mimosa',
+                thumb: 'https://www.thecocktaildb.com/images/media/drink/juhcuu1504370685.jpg',
+                glass: 'champagne flute',
+                instructions: 'pour champagne until the glass is mostly full, splash orange juice'
+            },
+            detailList,
+           
+        )
+        drink.addIngredient('champange');
+        drink.addIngredient('orange juice');
+        drink.generateRecipe();
+        const favoritesManager = new FavoritesManager(drink.id, drink.name);
+        favoritesManager.addToFavorites();
+        document.getElementById(`btn-${drink.id}`).click();
+        expect(localStorage.getItem('favoriteDrinks')).toContain('Mimosa');
+    })
 
-// //     it("should remove a drink from favorites list", function() {
-// //         const favoritesListBefore = getFavoritesList();
-// //         const drinkToRemove = favoritesListBefore[0];
-// //         removeFromFavorites(drinkToRemove.idDrink);
-// //         const favoritesListAfter = getFavoritesList();
-// //         const isInFavorites = favoritesListAfter.some(drink => drink.idDrink === drinkToRemove.idDrink);
-// //         expect(isInFavorites).toBeFalse();
-// //     });
-// // });
+    it('removes drink from favorites', function() {
+        const drink = new Drink({
+                id: 1234,
+                name: 'Mimosa',
+                thumb: 'https://www.thecocktaildb.com/images/media/drink/juhcuu1504370685.jpg',
+            },
+            detailList,
+        )
+        drink.generateDrink();
+        const favoritesManager = new FavoritesManager(drink.id, drink.name);
+        favoritesManager.removeFromFavorites();
+        document.getElementById(`btn-${drink.id}`).click();
+        expect(localStorage.getItem('favoriteDrinks')).not.toContain('Mimosa');
+    })
+});
